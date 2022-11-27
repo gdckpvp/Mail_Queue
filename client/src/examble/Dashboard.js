3185336a64cba1e0b4179e453e8aedd7d9739597
DuxTin <thinhdo2404@gmail.com>
2022-11-27 14:48:20 +0700

    del some import don't need to use

--- client/src/examble/Dashboard.js
+++ client/src/examble/Dashboard.js
@@ -5,19 +5,14 @@ import MuiDrawer from '@mui/material/Drawer';
 import Box from '@mui/material/Box';
 import MuiAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
-import Badge from '@mui/material/Badge';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Link from '@mui/material/Link';
-import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import Deposits from './Deposits';
 import Orders from './Orders';
 
 function Copyright(props) {
